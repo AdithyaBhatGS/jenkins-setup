be21793a0fa7e6933efa4c6586d9526e379aed70
Adithya Bhat G S <94121106+AdithyaBhatGS@users.noreply.github.com>
2025-11-12 15:11:37 +0530

    Modified the jenkins_setup.sh script by removing the openjdk11 and replacing it with openjdk21

--- scripts/jenkins_setup.sh
+++ scripts/jenkins_setup.sh
@@ -2,21 +2,19 @@
 set -e
 
 # Update system packages
-sudo apt-get update -y
+sudo apt update
 
 # Install Java (required by Jenkins)
-sudo apt-get install -y openjdk-11-jdk
+sudo apt install fontconfig openjdk-21-jre
 
 # Add Jenkins repository key and repo
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-# Install Jenkins
-sudo apt-get update -y
-sudo apt-get install -y jenkins git unzip
+sudo apt update
+sudo apt install jenkins
 
 # Start and enable Jenkins service
 sudo systemctl enable jenkins
