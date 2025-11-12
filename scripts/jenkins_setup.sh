6288db80cafe6ceb21a7622df0ea8c66cabd215e
Adithya Bhat G S <94121106+AdithyaBhatGS@users.noreply.github.com>
2025-11-12 15:13:08 +0530

    Added a comment for the line 18 and 19

--- scripts/jenkins_setup.sh
+++ scripts/jenkins_setup.sh
@@ -13,6 +13,8 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update and install the jenkins
 sudo apt update
 sudo apt install jenkins
 
